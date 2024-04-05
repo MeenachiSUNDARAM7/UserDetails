0e71f2bebabd6abf516ecb9e08bfef41c7054fa0
MeenachiSUNDARAM7 <meenakshisundaram2952002@gmail.com>
2024-04-05 23:55:02 +0530

    flyway db

---
+++ UserDetails/src/main/resources/db.migration/V1__create_user_details.sql
@@ -0,0 +1,7 @@
+CREATE TABLE user_details.user_entity (
+    id int AUTO_INCREMENT PRIMARY KEY,
+    name varchar(255),
+    email varchar(255),
+    phone_no varchar(255),
+    gender varchar(55)
+);
---
+++ UserDetails/src/main/resources/db.migration/V2__insert_user_details.sql
@@ -0,0 +1,3 @@
+insert into user_details.user_entity(id, name, email, phone_no, gender)
+values (1, 'Cristiano Ronaldo', 'cr7@gmail.com', '9987510236', 'male'),
+       (2, 'messi', 'messi@gmail.com', '7412589632', 'male');
